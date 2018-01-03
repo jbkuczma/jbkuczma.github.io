4a94220b362d8ca7e76ba7a0a9ca2b716fc42610
James <jbkuczma@buffalo.edu>
2018-01-03 15:11:06 -0500

    bug displaying images in prod

--- src/views/Photography.js
+++ src/views/Photography.js
@@ -6,13 +6,13 @@ class Photography extends Component {
     return (
       <section class="content">
         <p> You can view more of my photos on <a href="https://www.instagram.com/james.visual/" target="_blank" alt="instagram"> Instagram </a> and <a href="https://500px.com/jamesvisual" target="_blank" alt="500px"> 500px</a>.</p>        
-        {photos.map(photo => {
+        {/* {photos.map(photo => {
           return (
             <div class="photo-container" key={ photo.id }>
               <img src={ photo.src } alt="image" class="photo" />
             </div>
           )
-        })}
+        })} */}
       </section>
     );
   }
